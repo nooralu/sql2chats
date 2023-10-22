df34913478481cf1370e45da5c2c749d4a553af9
nooralu <nooralu@foxmail.com>
2023-10-22 19:14:36 +0800

    初始化后端项目

---
+++ serve/src/main/java/com/example/sql2chats/Sql2chatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sql2chats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql2chatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sql2chatsApplication.class, args);
+	}
+
+}
---
+++ serve/src/test/java/com/example/sql2chats/Sql2chatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql2chats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sql2chatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
